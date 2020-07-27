547d7dba8e5f4a90190821abe4cdca4366e6f95b
romina <casal.romina@gmail.com>
2020-07-27 01:51:02 -0300

    Set fcm_token collection name

--- src/models/fcm_token.py
+++ src/models/fcm_token.py
@@ -2,5 +2,6 @@ from src.conf.database import db
 
 
 class FCMToken(db.Document):
+    meta = {'collection': 'fcm_token'}
     user = db.StringField(required=True)
     tokens = db.ListField(db.StringField())
